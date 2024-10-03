d7ed85b0e5bed038136accda4fac361565bc9cf3
nemoth <tronghieu1042@gmail.com>
2024-10-03 10:11:18 +0700

    hot fix to test deploy

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -39,6 +39,6 @@ exports.createUser = async (req, res) => {
   }
 };
 
-exports.updateUser = async (req, res) => {}
+// exports.updateUser = async (req, res) => {}
 
-exports.deleteUser = async (req, res) => {}
+// exports.deleteUser = async (req, res) => {}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,7 +4,7 @@ const userController = require('../controllers/userControllers');
 
 router.get('/users', userController.getUser);
 router.post('/users', userController.createUser);
-router.put('/users/:id', userController.updateUserUser);
-router.delete('/users/:id', userController.deleteUser);
+// router.put('/users/:id', userController.updateUserUser);
+// router.delete('/users/:id', userController.deleteUser);
 
 module.exports = router;
