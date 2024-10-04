ff2b8de7da890999423b5a279e71e7067b586fc5
nemoth <tronghieu1042@gmail.com>
2024-10-04 11:50:49 +0700

    againn

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const express = require("express");
+const cors = require("cors");
 const connectDB = require("./config/db");
 
 const app = express();
